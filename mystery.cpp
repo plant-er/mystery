6acd0b7857b3cb93b3e32c25ba321f3771923489
plant-er <plant-er@users.noreply.github.com>
2019-02-27 19:14:19 -0400

    Added code for mysteries 2 and 3

--- mystery.cpp
+++ mystery.cpp
@@ -27,6 +27,38 @@ void mystery1(auto& Data)
 
 //... Other mysteries...
 
+//mystery 2 Selection Sort
+void mystery2(auto& Data)
+{
+  cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+
+  for ( int i = 0 ; i < Data.size( ) ; i++)
+  {
+    for ( int j = 0 ; j < Data.size() -1 ; j++)
+	if ( Data[ j+1 ] < Data[ j ] )
+	    swap( Data[ j+1] , Data[ j ] );
+
+    print(Data);
+  }//end outer for (this brace is needed to include the print statement)
+
+}
+
+//mystery 3 Insertion Sort
+void mystery3(auto& Data)
+{
+  cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+
+  for ( int i = 0 ; i < Data.size( ) ; i++)
+  {
+    for ( int j = i ; j < Data.size() ; j++)
+	if ( Data[ j ] < Data[ i] )
+	    swap( Data[ i ] , Data[ j ] );
+
+    print(Data);
+  }//end outer for (this brace is needed to include the print statement)
+
+}
+
 int main()
 {
     
